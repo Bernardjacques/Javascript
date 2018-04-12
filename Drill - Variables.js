cf1b233c6bd061fc444cf4714271fa7ea11bec71
Bernard Jacques <35732716+Bernardjacques@users.noreply.github.com>
2018-04-12 11:54:00 +0200

    Update Drill - Variables.js

--- Drill - Variables.js
+++ Drill - Variables.js
@@ -1,6 +1,23 @@
+// ===== Diseur de bonne aventure ========
+// ======= Variables =====================
+
 let metier = "ProGamer";
 let pays = "Belgique";
 let prenom = "Léa" ;
 let nbr_enfant = 2;
 
+// =======================================
+
 console.log("Vous serez", metier, "et habiterez à", pays, "et marié a", prenom, "avec", nbr_enfant, "enfants.")
+
+// =======================================
+
+
+// =========== Calculer l'âge ============
+// ======= Variables =====================
+
+let current_year = 2018;
+
+let birth_year = prompt("Entrez votre année de naissance");
+let age = current_year - birth_year;
+    console.log("Vous avez entre",age - 1, "et", age, "ans");
