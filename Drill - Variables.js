f04ccd6c832c09d12af3194c4e7cfebe19b30b17
Bernard Jacques <35732716+Bernardjacques@users.noreply.github.com>
2018-04-12 11:39:08 +0200

    Add files via upload

---
+++ Drill - Variables.js
@@ -0,0 +1,6 @@
+let metier = "ProGamer";
+let pays = "Belgique";
+let prenom = "Léa" ;
+let nbr_enfant = 2;
+
+console.log("Vous serez", metier, "et habiterez à", pays, "et marié a", prenom, "avec", nbr_enfant, "enfants.")
